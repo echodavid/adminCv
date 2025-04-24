219ac768024652a792cc561c8702e8c318eb4010
echodavid <despinosa2550@gmail.com>
2025-04-24 11:44:32 -0600

    fixing certificates and education

--- src/app/admin-certificates/admin-certificates.component.ts
+++ src/app/admin-certificates/admin-certificates.component.ts
@@ -9,6 +9,7 @@ import { CertificatesService } from '../services/certificates-service/certificat
   styleUrls: ['./admin-certificates.component.css']
 })
 export class AdminCertificatesComponent implements OnInit {
+  itemCount: number = 0;
   certificatesList: Certificates[] = [];
   newCertificate: Certificates = { name: '', institution: '', date: '', description: '' };
 
@@ -17,28 +18,30 @@ export class AdminCertificatesComponent implements OnInit {
   ngOnInit(): void {
     this.certificatesService.getCertificates().subscribe(data => {
       this.certificatesList = data;
+      this.itemCount = this.certificatesList.length;
     });
   }
 
   addCertificate() {
     if (this.newCertificate.name?.trim() && this.newCertificate.institution?.trim()) {
       this.certificatesService.createCertificates(this.newCertificate).then(() => {
-        console.log('Certificado agregado exitosamente');
+        console.log('Certificate added successfully');
         this.newCertificate = { name: '', institution: '', date: '', description: '' };
+        this.itemCount = this.certificatesList.length;
       }).catch((error: any) => {
-        console.error('Error al agregar el certificado:', error);
+        console.error('Error adding certificate:', error);
       });
     } else {
-      console.error('El nombre y la institución no pueden estar vacíos');
+      console.error('The name and institution cannot be empty');
     }
   }
 
   deleteCertificate(id: string) {
- 
     this.certificatesService.deleteCertificates(id).then(() => {
-      console.log('Certificado eliminado exitosamente');
+      console.log('Certificate deleted successfully');
+      this.itemCount = this.certificatesList.length;
     }).catch(error => {
-      console.error('Error al eliminar el certificado:', error);
+      console.error('Error deleting certificate:', error);
     });
   }
 }
